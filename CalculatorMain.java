c62e8e207cd97a684ccaabb610e747b57e5602a8
DESKTOP-RN2K5O7\Yashwanth <gupta.yashwanth@gmail.com>
2018-03-16 03:13:01 +0100

    cdfs

---
+++ CalculatorMain.java
@@ -0,0 +1,26 @@
+package calc;
+
+public class CalculatorMain {
+	public CalculatorMain(){
+		
+	}
+	public int add(int a, int b){
+		return a+b;
+	}
+	public int sub(int a, int b){
+		return a-b;
+	}
+	public int mul(int a, int b){
+		return a*b;
+	}
+	public int div(int a, int b){
+		if(b==0)
+			return b;
+		else
+		return a/b;
+	}
+	public int pwr(int a, int b){
+		return (int)Math.pow(3.0,2.0);
+	}
+	
+}
